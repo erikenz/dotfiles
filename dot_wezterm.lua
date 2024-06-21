1e0b50ae31c02fff5eb8fbff0fc3fd9132b73001
erikenz <46247842+erikenz@users.noreply.github.com>
2024-06-21 18:24:34 -0300

    Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -7,7 +7,13 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "AdventureTime"
+config.color_scheme = "Catppuccin Mocha"
+
+config.font = wezterm.font("CaskaydiaCove Nerd Font")
+-- You can specify some parameters to influence the font selection;
+-- for example, this selects a Bold, Italic font variant.
+-- config.font =
+--   wezterm.font('JetBrains Mono', { weight = 'Bold', italic = true })
 
 -- and finally, return the configuration to wezterm
 return config
