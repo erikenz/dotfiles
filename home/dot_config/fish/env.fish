c72169a405f187478e41346bbe33eb9df4f4d4bb
erikenz <46247842+erikenz@users.noreply.github.com>
2025-07-25 00:53:10 -0300

    Add home/.chezmoiscripts/arch/10-install-paru.sh Add home/.chezmoiscripts/arch/20-install-packages.sh Update home/.config/fish/env.fish Update home/.config/fish/fish_variables

--- home/dot_config/fish/env.fish
+++ home/dot_config/fish/env.fish
@@ -1,2 +1,15 @@
 set -xg TERMINAL ghostty
 set -xg EDITOR nvim
+
+### GO LANG
+set -x GOPATH $HOME/go
+set -x PATH $PATH $GOPATH/bin
+# if test -d ~/go
+#     set --export --universal GOROOT (go env GOROOT)
+#     set --export --universal GOPATH (go env GOPATH)
+#     set --export --universal GOBIN (go env GOBIN)
+#     set --export --universal PATH $PATH $GOBIN $GOROOT/bin
+#     alias cdgo="cd $GOPATH/src"
+#     alias gomodOn="set -gx GO111MODULE on"
+#     alias gomodOff="set -gx GO111MODULE off"
+# end
