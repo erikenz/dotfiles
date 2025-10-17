aed069598054a3dd6672ca86ec602c55ba9ebccc
erikenz <46247842+erikenz@users.noreply.github.com>
2025-10-17 14:49:15 -0300

    Update .config/fish/aliases.fish Update .config/nvim-lazyvim/lazy-lock.json Update .config/nvim-lazyvim/lazyvim.json Update .config/nvim-lazyvim/lua/config/keymaps.lua Update .config/nvim-lazyvim/lua/config/options.lua Update .config/YouTube Music/config.json Update .config/zed/keymap.json Update .config/zed/settings.json Update .config/zed/tasks.json

--- home/dot_config/nvim-lazyvim/lua/config/keymaps.lua
+++ home/dot_config/nvim-lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,96 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Map Ctrl+b in insert mode to delete to the end of the word without leaving insert mode
+-- vim.keymap.set("i", "<C-b>", "<C-o>de")
+
+-- Map Ctrl+c to escape from other modes
+-- vim.keymap.set({ "i", "n", "v" }, "<C-c>", [[<C-\><C-n>]])
+
+-- Screen Keys
+-- vim.keymap.set({ "n" }, "<leader>uk", "<cmd>Screenkey<CR>")
+
+----- Tmux Navigation ------
+local nvim_tmux_nav = require("nvim-tmux-navigation")
+
+vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft) -- Navigate to the left pane
+vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown) -- Navigate to the bottom pane
+vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp) -- Navigate to the top pane
+vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight) -- Navigate to the right pane
+vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive) -- Navigate to the last active pane
+vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext) -- Navigate to the next pane
+
+-- ############################################################################
+--                         Begin of markdown section
+-- ############################################################################
+
+----- OBSIDIAN -----
+vim.keymap.set("n", "<leader>oc", "<cmd>ObsidianCheck<CR>", { desc = "Obsidian Check Checkbox" })
+vim.keymap.set("n", "<leader>ot", "<cmd>ObsidianTemplate<CR>", { desc = "Insert Obsidian Template" })
+vim.keymap.set("n", "<leader>oo", "<cmd>Obsidian Open<CR>", { desc = "Open in Obsidian App" })
+vim.keymap.set("n", "<leader>ob", "<cmd>ObsidianBacklinks<CR>", { desc = "Show ObsidianBacklinks" })
+vim.keymap.set("n", "<leader>ol", "<cmd>ObsidianLinks<CR>", { desc = "Show ObsidianLinks" })
+vim.keymap.set("n", "<leader>on", "<cmd>ObsidianNew<CR>", { desc = "Create New Note" })
+vim.keymap.set("n", "<leader>os", "<cmd>ObsidianSearch<CR>", { desc = "Search Obsidian" })
+vim.keymap.set("n", "<leader>oq", "<cmd>ObsidianQuickSwitch<CR>", { desc = "Quick Switch" })
+
+-- Keymap to switch spelling language to English lamw25wmal
+-- To save the language settings configured on each buffer, you need to add
+-- "localoptions" to vim.opt.sessionoptions in the `lua/config/options.lua` file
+vim.keymap.set("n", "<leader>msle", function()
+  vim.opt.spelllang = "en"
+  vim.cmd("echo 'Spell language set to English'")
+end, { desc = "Spelling language English" })
+
+-- Keymap to switch spelling language to Spanish lamw25wmal
+vim.keymap.set("n", "<leader>msls", function()
+  vim.opt.spelllang = "es"
+  vim.cmd("echo 'Spell language set to Spanish'")
+end, { desc = "Spelling language Spanish" })
+
+-- Keymap to switch spelling language to German lamw25wmal
+vim.keymap.set("n", "<leader>mslg", function()
+  vim.opt.spelllang = "de"
+  vim.cmd("echo 'Spell language set to German'")
+end, { desc = "Spelling language German" })
+
+-- Keymap to switch spelling language to both spanish and english lamw25wmal
+vim.keymap.set("n", "<leader>mslb", function()
+  vim.opt.spelllang = "en,es"
+  vim.cmd("echo 'Spell language set to Spanish and English'")
+end, { desc = "Spelling language Spanish and English" })
+
+-- Show spelling suggestions / spell suggestions
+-- NOTE: I changed this to accept the first spelling suggestion
+vim.keymap.set("n", "<leader>mss", function()
+  -- Simulate pressing "z=" with "m" option using feedkeys
+  -- vim.api.nvim_replace_termcodes ensures "z=" is correctly interpreted
+  -- 'm' is the {mode}, which in this case is 'Remap keys'. This is default.
+  -- If {mode} is absent, keys are remapped.
+  --
+  -- I tried this keymap as usually with
+  vim.cmd("normal! 1z=")
+  -- But didn't work, only with nvim_feedkeys
+  -- vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("z=", true, false, true), "m", true)
+end, { desc = "Spelling suggestions" })
+
+-- markdown good, accept spell suggestion
+-- Add word under the cursor as a good word
+vim.keymap.set("n", "<leader>msg", function()
+  vim.cmd("normal! zg")
+  -- I do a write so that harper is updated
+  vim.cmd("silent write")
+end, { desc = "Spelling add word to spellfile" })
+
+-- Undo zw, remove the word from the entry in 'spellfile'.
+vim.keymap.set("n", "<leader>msu", function()
+  vim.cmd("normal! zug")
+end, { desc = "[P]Spelling undo, remove word from list" })
+
+-- Repeat the replacement done by |z=| for all matches with the replaced word
+-- in the current window.
+vim.keymap.set("n", "<leader>msr", function()
+  -- vim.cmd(":spellr")
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(":spellr\n", true, false, true), "m", true)
+end, { desc = "[P]Spelling repeat" })
--- home/dot_config/nvim-lazyvim/lua/config/options.lua
+++ home/dot_config/nvim-lazyvim/lua/config/options.lua
@@ -1,3 +1,40 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- vim.lsp.enable
+
+-- I added `localoptions` to save the language spell settings, otherwise, the
+-- language of my markdown documents was not remembered if I set it to spanish
+-- or to both en,es
+-- See the help for `sessionoptions`
+-- `localoptions`: options and mappings local to a window or buffer
+-- (not global values for local options)
+--
+-- The plugin that saves the session information is
+-- https://github.com/folke/persistence.nvim and comes enabled in the
+-- lazyvim.org distro lamw25wmal
+--
+-- These sessionoptions come from the lazyvim distro, I just added localoptions
+-- https://www.lazyvim.org/configuration/general
+vim.opt.sessionoptions = {
+  "buffers",
+  "curdir",
+  "tabpages",
+  "winsize",
+  "help",
+  "globals",
+  "skiprtp",
+  "folds",
+  "localoptions",
+}
+
+-- Most of my files have 2 languages, spanish and english, so even if I set the
+-- language to spanish, I always add some words in English to my documents, so
+-- it's annoying to be adding those to the spanish dictionary
+-- vim.opt.spelllang = { "en,es" }
+
+-- I mainly type in english, if I set it to both above, files in English get a
+-- bit confused and recognize words in spanish, just for spanish files I need to
+-- set it to both
+vim.opt.spelllang = { "en" }
