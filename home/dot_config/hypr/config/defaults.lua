d2d4b9e321e410eac7e179ee20ff71082f19644d
erikenz <erikenz2001@gmail.com>
2026-06-07 06:01:55 -0300

    Add .config/hypr/bin/launch-or-focus Add .config/hypr/bin/launch-or-focus-tui Add .config/hypr/bin/launch-or-focus-webapp Add .config/hypr/bin/launch-webapp Add .config/hypr/config/animations.lua Add .config/hypr/config/autostart.lua Add .config/hypr/config/colors.lua Add .config/hypr/config/decorations.lua Add .config/hypr/config/defaults.lua Add .config/hypr/config/environment.lua Add .config/hypr/config/input.lua Add .config/hypr/config/keybinds.lua Add .config/hypr/config/keybinds/apps.lua Add .config/hypr/config/keybinds/media.lua Add .config/hypr/config/keybinds/navigation.lua Add .config/hypr/config/keybinds/utilities.lua Add .config/hypr/config/misc.lua Add .config/hypr/config/monitors.lua Add .config/hypr/config/noctalia-colors.lua Add .config/hypr/config/windowrules.lua Add .config/hypr/hyprland.conf Add .config/hypr/hyprland.lua Add .config/hypr/luarc.json Add .config/hypr/noctalia/noctalia-colors.conf Add .config/hypr/xdph.conf

---
+++ home/dot_config/hypr/config/defaults.lua
@@ -0,0 +1,7 @@
+-- Hyprland default apps
+
+TERMINAL = "ghostty"
+FILE_MANAGER = "dolphin"
+BROWSER = "vivaldi"
+EDITOR = "nvim"
+CALCULATOR = "gnome-calculator"
---
+++ home/dot_config/hypr/config/keybinds.lua
@@ -0,0 +1,7 @@
+-- Keybindings Main Entry Point
+-- Load modular keybinding configurations
+
+require("config.keybinds.navigation")
+require("config.keybinds.apps")
+require("config.keybinds.utilities")
+require("config.keybinds.media")
