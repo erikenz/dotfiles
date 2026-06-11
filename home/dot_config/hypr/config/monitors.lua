d68aefbb11147452c4d5274e355df11c9a3c1f0a
erikenz <erikenz2001@gmail.com>
2026-06-10 23:40:30 -0300

    Update .config/hypr/config/monitors.lua Update .config/noctalia/plugins/keybind-cheatsheet/settings.json Update .config/noctalia/plugins/screen-toolkit/settings.json Update .config/noctalia/settings.json Update .config/zed/settings.json

--- home/dot_config/hypr/config/monitors.lua
+++ home/dot_config/hypr/config/monitors.lua
@@ -4,7 +4,13 @@ hl.monitor({ output = "HDMI-A-1", mode = "1920x1080", position = "0x0", scale = 
 hl.monitor({ output = "eDP-1", mode = "1920x1080@144", position = "1920x0", scale = 1 })
 
 -- Workspace Monitor Rules
-hl.workspace_rule({ workspace = "10", monitor = "eDP-1" })
-hl.workspace_rule({ workspace = "9", monitor = "HDMI-A-1" })
+hl.workspace_rule({ workspace = "1", persistent = true })
+hl.workspace_rule({ workspace = "2", persistent = true })
+hl.workspace_rule({ workspace = "3", persistent = true })
+hl.workspace_rule({ workspace = "4", persistent = true })
+hl.workspace_rule({ workspace = "5", persistent = true })
+hl.workspace_rule({ workspace = "6", persistent = true })
+hl.workspace_rule({ workspace = "7", persistent = true })
 hl.workspace_rule({ workspace = "8", monitor = "HDMI-A-1" })
-
+hl.workspace_rule({ workspace = "9", monitor = "HDMI-A-1" })
+hl.workspace_rule({ workspace = "10", monitor = "eDP-1" })
