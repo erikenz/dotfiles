c2a8a67ee75a54db824f6898354368eaae64ff4f
erikenz <erikenz2001@gmail.com>
2026-08-13 13:26:42 -0300

    Update .config/astronvim/lazy-lock.json Update .config/fish/config.fish Update .config/fish/functions/__gitnow_load_git_functions.fish Update .config/fish/fish_variables

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -1,22 +1,25 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
-# overwrite greeting
-# potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
-
 # Initialize Starship prompt
 starship init fish | source
 
 # Initialize mise tool manager
 mise activate fish | source
 
+# Convenience 'n' function
 function n
     if test (count $argv) -eq 0
         nvim .
     else
         nvim $argv
+    end
+end
+
+# Interactive Neovim Distro Selector (requires fzf)
+function nvims
+    set -l config (string split " " "astronvim lazyvim" | fzf --prompt=" Select Neovim Config ❯ " --height=~40% --layout=reverse --border)
+    if test -n "$config"
+        env NVIM_APPNAME=$config nvim $argv
     end
 end
 
@@ -32,17 +35,18 @@ alias b='bun'
 alias bx='bunx'
 alias npm=pnpm
 
-# dotfiles
+# Dotfiles
 alias conf-fish='cd ~/.config/fish && n'
 alias conf-ghostty='cd ~/.config/ghostty && n'
 alias conf-hypr='cd ~/.config/hypr && n'
-alias conf-nvim='cd ~/.config/nvim && n'
-alias conf-astronvim='cd ~/.config/astronvim && n'
-alias conf-lazyvim='cd ~/.config/lazyvim && n'
+alias conf-astronvim='cd ~/.config/astronvim && astronvim .'
+alias conf-lazyvim='cd ~/.config/lazyvim && lazyvim .'
+alias conf-nvim='conf-astronvim'
 
-# nvim
-alias astronvim='NVIM_APPNAME=astronvim nvim'
-alias lazyvim='NVIM_APPNAME=lazyvim nvim'
+# Neovim Distros (using 'env' for Fish compatibility)
+alias astronvim='env NVIM_APPNAME=astronvim nvim'
+alias lazyvim='env NVIM_APPNAME=lazyvim nvim'
+alias nvim='astronvim' # Default 'nvim' command calls AstroNvim
 
 alias portainer='docker run -p 8000:8000 -p 9443:9443 --name portainer -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts'
 
@@ -54,4 +58,3 @@ set -gx PNPM_HOME "/home/erikzen/.local/share/pnpm"
 if not string match -q -- "$PNPM_HOME/bin" $PATH
     set -gx PATH "$PNPM_HOME/bin" $PATH
 end
-# pnpm end
