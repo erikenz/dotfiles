8f8ebc3aaeed7d31af0cefcadd54a070524b48ec
erikenz <erikenz2001@gmail.com>
2026-08-13 12:02:43 -0300

    Add .config/astronvim/README.md Add .config/astronvim/.luarc.json Add .config/astronvim/.neoconf.json Add .config/astronvim/.stylua.toml Add .config/astronvim/init.lua Add .config/astronvim/lazy-lock.json Add .config/astronvim/lua/community.lua Add .config/astronvim/lua/lazy_setup.lua Add .config/astronvim/lua/plugins/astrocore.lua Add .config/astronvim/lua/plugins/astrolsp.lua Add .config/astronvim/lua/plugins/astroui.lua Add .config/astronvim/lua/plugins/mason.lua Add .config/astronvim/lua/plugins/none-ls.lua Add .config/astronvim/lua/plugins/treesitter.lua Add .config/astronvim/lua/plugins/user.lua Add .config/astronvim/lua/polish.lua Add .config/astronvim/neovim.yml Add .config/astronvim/selene.toml

---
+++ home/dot_config/astronvim/lua/community.lua
@@ -0,0 +1,47 @@
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+
+  -- AI
+  { import = "astrocommunity.ai.avante-nvim" },
+
+  -- Bars and Lines
+  { import = "astrocommunity.bars-and-lines.dropbar-nvim" },
+
+  -- Diagnostics
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+
+  -- Editing Support
+  { import = "astrocommunity.editing-support.multiple-cursors-nvim" },
+
+  -- File Explorer
+  { import = "astrocommunity.file-explorer.oil-nvim" },
+
+  -- Git
+  { import = "astrocommunity.git.diffview-nvim" },
+
+  -- Markdown and LaTeX
+  { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
+
+  -- Motion
+  { import = "astrocommunity.motion.flash-nvim" },
+
+  -- Pack
+  { import = "astrocommunity.pack.biome" },
+  { import = "astrocommunity.pack.chezmoi" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.fish" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.mdx" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.yaml" },
+
+  -- Search
+  { import = "astrocommunity.search.grug-far-nvim" },
+}
---
+++ home/dot_config/astronvim/lua/plugins/astrocore.lua
@@ -0,0 +1,67 @@
+-- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
+-- Configuration documentation can be found with `:h astrocore`
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    -- Configure core features of AstroNvim
+    features = {
+      large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+      autopairs = true, -- enable autopairs at start
+      cmp = true, -- enable completion at start
+      diagnostics = { virtual_text = true, virtual_lines = false }, -- diagnostic settings on startup
+      highlighturl = true, -- highlight URLs at start
+      notifications = true, -- enable notifications at start
+    },
+    -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
+    diagnostics = {
+      virtual_text = true,
+      underline = true,
+    },
+    -- vim options can be configured here
+    options = {
+      opt = { -- vim.opt.<key>
+        clipboard = "unnamedplus", -- sync system clipboard
+        relativenumber = true, -- sets vim.opt.relativenumber
+        number = true, -- sets vim.opt.number
+        spell = false, -- sets vim.opt.spell
+        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
+        wrap = false, -- sets vim.opt.wrap
+      },
+      g = { -- vim.g.<key>
+        -- OSC 52 Clipboard provider for remote SSH / herdr sessions
+        clipboard = {
+          name = "OSC 52",
+          copy = {
+            ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+            ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+          },
+          paste = {
+            ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+            ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+          },
+        },
+      },
+    },
+    -- Mappings can be configured through AstroCore as well.
+    mappings = {
+      n = {
+        -- navigate buffer tabs
+        ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
+        ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+
+        -- mappings seen under group name "Buffer"
+        ["<Leader>bd"] = {
+          function()
+            require("astroui.status.heirline").buffer_picker(
+              function(bufnr) require("astrocore.buffer").close(bufnr) end
+            )
+          end,
+          desc = "Close buffer from tabline",
+        },
+      },
+    },
+  },
+}
